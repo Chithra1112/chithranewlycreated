5ec77a41da0350976b58e86b869ae7ee5cd82749
SIJU <SIJU@DESKTOP-LAFFKET>
2023-06-09 14:32:05 +0530

    interface1

---
+++ newproject1/src/main/java/abstractbyinterface/A6.java
@@ -0,0 +1,14 @@
+package abstractbyinterface;
+
+public class A6 implements Printable {
+public void print()
+{
+	System.out.println("Hello");
+}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+A6 obj=new A6();
+obj.print();
+	}
+
+}
---
+++ newproject1/src/main/java/abstractbyinterface/Printable.java
@@ -0,0 +1,5 @@
+package abstractbyinterface;
+
+public interface Printable {
+void print();
+}
