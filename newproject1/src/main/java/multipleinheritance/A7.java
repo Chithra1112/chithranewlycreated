3b59397a86ab09cac21c527b07a4890598bc10e2
SIJU <SIJU@DESKTOP-LAFFKET>
2023-06-12 14:20:13 +0530

    multipleinheritance

---
+++ newproject1/src/main/java/multipleinheritance/A7.java
@@ -0,0 +1,19 @@
+package multipleinheritance;
+
+public class A7 implements Printable,Showable{
+public void print()
+{
+	System.out.println("Hello");
+}
+public void show()
+{
+	System.out.println("Welcome");
+}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+A7 obj=new A7();
+obj.print();
+obj.show();
+	}
+
+}
