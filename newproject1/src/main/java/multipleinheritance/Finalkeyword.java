97678e19d9c7002625eddc17db2a8a436a9c8109
SIJU <SIJU@DESKTOP-LAFFKET>
2023-06-15 09:21:49 +0530

    finalkeyword in method

---
+++ newproject1/src/main/java/multipleinheritance/Finalkeyword.java
@@ -0,0 +1,13 @@
+package multipleinheritance;
+
+public class Finalkeyword {
+final void show()
+{
+	System.out.println("My java");
+	}
+	
+		// TODO Auto-generated method stub
+
+	}
+
+
---
+++ newproject1/src/main/java/multipleinheritance/Finalkeyword1.java
@@ -0,0 +1,14 @@
+package multipleinheritance;
+
+public class Finalkeyword1 extends Finalkeyword{
+void show()
+{
+	System.out.println("Java");
+}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Finalkeyword1 obj=new Finalkeyword1();
+obj.show();
+	}
+
+}
