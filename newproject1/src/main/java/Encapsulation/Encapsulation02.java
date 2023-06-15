d4fe0e33a3f5763db82af34c8f0060071aa93a52
SIJU <SIJU@DESKTOP-LAFFKET>
2023-06-15 10:09:44 +0530

    encapsulation2

---
+++ newproject1/src/main/java/Encapsulation/Encapsulation02.java
@@ -0,0 +1,11 @@
+package Encapsulation;
+
+public class Encapsulation02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Encapsulation2 obj=new Encapsulation2();
+System.out.println("name is " +obj.getName());
+	}
+
+}
---
+++ newproject1/src/main/java/Encapsulation/Encapsulation2.java
@@ -0,0 +1,9 @@
+package Encapsulation;
+
+public class Encapsulation2 {
+private String name="Chithra";
+
+public String getName() {
+	return name;
+}
+}
