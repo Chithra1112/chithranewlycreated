821b10d854a8fea87772e3109c34e5c399f909a8
SIJU <SIJU@DESKTOP-LAFFKET>
2023-06-15 10:08:48 +0530

    encapsulation1

---
+++ newproject1/src/main/java/Encapsulation/Employee2.java
@@ -0,0 +1,14 @@
+package Encapsulation;
+
+public class Employee2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Employee1 obj = new Employee1();
+		obj.setName("Chithra");
+		obj.setSalary(1000);
+		System.out.println("name is " + obj.getName());
+		System.out.println("salary is " + obj.getSalary());
+	}
+
+}
