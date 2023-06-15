5cbc2c36d91f61e50e1bbfb43077760f99b3518b
SIJU <SIJU@DESKTOP-LAFFKET>
2023-06-15 10:03:10 +0530

    final keyword in class

---
+++ newproject1/src/main/java/multipleinheritance/Finalkeyword02.java
@@ -0,0 +1,15 @@
+package multipleinheritance;
+
+public class Finalkeyword02 extends Finalkeyword2 {
+	void show() {
+		System.out.println("hi java");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Finalkeyword02 obj = new Finalkeyword02();
+		obj.show();
+		obj.sample();
+	}
+
+}
---
+++ newproject1/src/main/java/multipleinheritance/Finalkeyword2.java
@@ -0,0 +1,8 @@
+package multipleinheritance;
+
+final public class Finalkeyword2 {
+void sample()
+{
+System.out.println("JAVA WORLD");	
+}
+}
